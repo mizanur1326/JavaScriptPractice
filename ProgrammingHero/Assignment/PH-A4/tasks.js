fec2f0116e7cfbc1a4f7940ace972a81d5c0b3a8
mizanur1326 <Iamuser@94>
2024-02-08 18:25:16 +0600

    problem 4 done

--- ProgrammingHero/Assignment/PH-A4/tasks.js
+++ ProgrammingHero/Assignment/PH-A4/tasks.js
@@ -29,4 +29,42 @@ function checkName(name){
         
     return lastName;
 }
-console.log(checkName(""))
+// console.log(checkName("MizanurA"))
+
+// Task 3
+function deleteInvalids() {
+    const nums = [NaN, 'a', '5', 5,  1, 12, 0, -1, undefined]
+    if (!Array.isArray(nums)) {
+        return "Invalid Array!";
+    }
+
+    return nums.filter(function(validation) {
+        return typeof validation === 'number' && !isNaN(validation);
+    });
+    return deleteInvalids();
+}
+
+// console.log(deleteInvalids());
+
+//Task 4
+function password(info) {
+    if (!info.name || !info.siteName || !info.birthYear || info.birthYear.toString().length !== 4) {
+        return "Invalid!";
+    }
+
+    // Capitalize the first letter of siteName
+    const upperCase = info.siteName.charAt(0).toUpperCase() + info.siteName.slice(1);
+
+    const password = `${upperCase}#${info.name}@${info.birthYear}`;
+    return password;
+}
+
+// console.log(password({ siteName: 'google', name: "Rahim", birthYear: 2000 }));
+
+// Task 5
+
+
+
+
+
+
